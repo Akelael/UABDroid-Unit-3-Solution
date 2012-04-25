d57b40c4310a5fe301ee4c25312622d85e1da33e
Rubén Serrano <akelael@gmail.com>
2012-04-25 10:41:38 +0200

    Versio incial

--- gen/org/uab/deic/uabdroid/solutions/unit3/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.uab.deic.uabdroid.solutions.unit3;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
